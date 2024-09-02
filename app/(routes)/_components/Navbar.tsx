ceefe88ebd3114ee28a733e0434592af1e7296cf
rmkofficial <kmert0@outlook.com>
2024-09-02 10:34:01 +0300

    Update Navbar component and dependencies

--- app/(routes)/_components/Navbar.tsx
+++ app/(routes)/_components/Navbar.tsx
@@ -1,6 +1,11 @@
 import { ModeToggle } from "@/components/ModeToggle";
+import { User } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import UserToggle from "./UserToggle";
+import MobileMenu from "./MobileMenu";
+import { NavMenu } from "@/constants";
+import NavItem from "./NavItem";
 
 const Navbar = () => {
   return (
@@ -17,10 +22,16 @@ const Navbar = () => {
             />
           </div>
           <div className="flex-row lg:flex hidden items-center gap-9 ml-auto">
-            Home About
+            {NavMenu.map((item) => (
+              <NavItem key={item.title} title={item.title} url={item.url} />
+            ))}
           </div>
           <div className="flex lg-flex items-center ml-auto gap-2">
+            <UserToggle />
             <ModeToggle />
+            <div className="lg:hidden flex flex-row items-center ml-auto gap-2">
+              <MobileMenu />
+            </div>
           </div>
         </div>
       </div>
